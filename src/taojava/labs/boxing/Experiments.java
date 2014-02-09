69d42e63cc4ed503986aca53f17dfda3c79615fe
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-02-09 16:26:59 -0600

    Add first version of code.

---
+++ src/taojava/labs/boxing/Experiments.java
@@ -0,0 +1,111 @@
+package taojava.labs.boxing;
+
+/**
+ * A few procedures to use in exploring automatic boxing and unboxing 
+ * in Java.
+ */
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class Experiments
+{
+  // +----------------+----------------------------------------------------
+  // | Static Methods |
+  // +----------------+
+
+  /**
+   * Add brackets around a string.
+   */
+  public static String
+  bracket(String str)
+  {
+    return "[" + str + "]";
+  } // bracket
+
+  /**
+   * Add parentheses around a stringified object.
+   */
+  public static String
+  parenthesize(Object o)
+  {
+    return "(" + o.toString() + ")";
+  } // parenthesize(Object)
+
+  /**
+   * Square an Integer.
+   */
+  public static Integer 
+  square(Integer x)
+  {
+    int val = x.intValue(); 
+    return new Integer(val * val);
+  } // square(Integer)
+
+  /**
+   * Square an int.
+   */
+  public static int
+  sqr(int x)
+  {
+    return x * x;
+  } // sqr
+
+  /**
+   * Sum an array of ints.
+   */
+  public static int
+  littleSum(int[] values)
+  {
+    int sum = 0;
+    for(int i = 0; i < values.length; i++)
+      {
+        sum += values[i];
+      } // for
+    return sum;
+  } // littleSum(int[])
+
+  /**
+   * Sum an array of Integers.
+   */
+  public static Integer
+  bigSum(Integer[] values)
+  {
+    int sum = 0;
+    for(int i = 0; i < values.length; i++)
+      {
+        sum += values[i].intValue();
+      } // for
+    return new Integer(sum);
+  } // bigSum(Integer[])
+
+  // +------+--------------------------------------------------------------
+  // | Main |
+  // +------+
+
+  public static void 
+  main(String[] args)
+      throws Exception
+  {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    Integer bigi = new Integer(5);
+    int littlei = 3;
+    String str = "Hello";
+    int[] littlevals = new int[] { 1, 2, 3 };
+    Integer[] bigvals = new Integer[] { new Integer(4),
+                                        new Integer(5),
+                                        new Integer(6) };
+
+    // Print out our original values.
+    pen.println("bigi: " + bigi);
+    pen.println("littlei: " + littlei);
+    pen.println("str: " + str);
+    pen.println("bigvals: " + bigvals + " or " + Arrays.toString(bigvals));
+    pen.println("littlevals: " + littlevals + " or " 
+                + Arrays.toString(bigvals));
+    pen.println();
+
+    // We're done.  Clean up.
+    pen.close();
+  } // main(String[])
+} // class Experiments
